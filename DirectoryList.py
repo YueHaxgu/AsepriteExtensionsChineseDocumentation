88b1b6f91083a7ab50a2d4562ca36f7b08d74ab0
是66呀 <2652915018@qq.com>
2024-05-14 13:36:29 +0800

    修改py

--- DirectoryList.py
+++ DirectoryList.py
@@ -9,15 +9,7 @@ print(entries)
 # 指定文件路径
 file_path = './mds.txt'
 
-# 检查文件是否存在
-if not os.path.exists(file_path):
-    # 文件不存在，创建文件
-    with open(file_path, 'w') as file:
-        file.write('')  # 创建一个空文件
-        with open(file_path, 'w',encoding='utf-8') as file:
-           for entry in entries:
-                file.write(entry + '\n')  # 添加换行符确保每个条目在新的一行
-else:
-    with open(file_path, 'w',encoding='utf-8') as file:
-           for entry in entries:
-                file.write(entry + '\n')  # 添加换行符确保每个条目在新的一行
+
+with open(file_path, 'w',encoding='utf-8') as file:
+    for entry in entries:
+        file.write(entry + '\n')  # 添加换行符确保每个条目在新的一行
