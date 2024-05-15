86d9faeec5fd2684f93803f35aea26c6cd792c1e
是66呀 <2652915018@qq.com>
2024-05-15 15:13:43 +0800

    修正py脚本

--- DirectoryList.py
+++ DirectoryList.py
@@ -6,6 +6,8 @@ path = 'mdDocumentation'
 # 获取指定目录中的文件和目录列表，只包括以 .md 结尾的文件
 entries = [entry for entry in os.listdir(path) if entry.endswith('.md')]
 print(entries)
+with open(file_path, 'w',encoding='utf-8') as file:
+    file.write("")  #初始化文件内容为空
 for entry in entries:
     with open(path + '/' + entry, 'r',encoding='utf-8') as file:
         file_content = file.read()
@@ -23,15 +25,14 @@ for entry in entries:
                 print("链接：", match[1])
                 author_name = match[0]
                 author_href = match[1]
-  
+                with open(file_path, 'a',encoding='utf-8') as file:
+                    file.write( entry + " #" +author_name  + " @" + author_href + '\n' )  # 添加换行符确保每个条目在新的一行
         else:
             
             print("未找到作者信息")
             # 写入作者信息和文件名到文件
-        with open(file_path, 'w',encoding='utf-8') as file:
-            for entry in entries:
-                file.write( entry + " #" +author_name  + " @" + author_href + '\n' )  # 添加换行符确保每个条目在新的一行
-# 指定文件路径
+        
+
 
 
 
